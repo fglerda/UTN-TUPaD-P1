a6ac84067455bc40e738af67fd5fda78a1ca9c8d
flerda <fglerda@gmail.com>
2025-04-12 13:18:51 -0300

    carga de ejercicios faltantes y eliminacion de documento temporal.

--- 04 Estructuras Repetitivas/practico4_estructuras_repetitivas.py
+++ 04 Estructuras Repetitivas/practico4_estructuras_repetitivas.py
@@ -83,3 +83,76 @@ entre 0 y 100, en orden decreciente."""
 
 for i in range(100, -1, -2):  # Incluyendo el 100 y el 0.
     print(i)
+
+"""7) Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
+número entero positivo indicado por el usuario."""
+
+print("Suma de numeros desde 0 hasta numero indicado por el usuario.")
+numero_usuario = int(input("Ingrese un numero para operar: "))
+suma = 0
+if numero_usuario >= 0:
+    for i in range(numero_usuario + 1):  # Incluye numero ingresado por usuario
+        suma += i
+elif numero_usuario < 0:
+    for i in range(0, numero_usuario - 1, -1):  # Incluye numero ingresado por usuario
+        suma += i
+print("La suma total es:", suma)
+
+"""8) Escribe un programa que permita al usuario ingresar 100 números enteros. Luego, el
+programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son
+negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad
+menor, pero debe estar preparado para procesar 100 números con un solo cambio)."""
+
+import random
+
+par = 0
+impar = 0
+positivo = 0
+negativo = 0
+
+for i in range(100):
+    numero = int(
+        input("Ingrese numero: ")
+    )  # numero = random.randint(-200, 200) #Se reemplaza esta linea por el comentario para probar el codigo.
+    if numero % 2 == 0:
+        par += 1
+    else:
+        impar += 1
+    if numero > 0:
+        positivo += 1
+    else:
+        negativo += 1
+print(
+    f"Numeros ingresados:\nNumeros pares: {par}\nNumeros impares: {impar}\nnumeros positivos: {positivo}\nNumeros negativos: {negativo}"
+)
+
+"""9) Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la
+media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe
+poder procesar 100 números cambiando solo un valor)."""
+
+import random
+
+suma = 0
+for i in range(100):
+    numero = int(
+        input("Ingrese numero: ")
+    )  # numero = random.randint(-100, 100)#se usa para probar el codigo.
+    suma = suma + numero
+    media = suma / 100
+print(f"La suma total es: {suma}")
+print(f"La media es: {media}")
+
+"""10) Escribe un programa que invierta el orden de los dígitos de un número ingresado por el
+usuario. Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745."""
+
+numero = abs(int(input("Ingrese un numero: ")))
+num_invertido = 0
+while numero > 0:
+    digito = numero % 10  # Obtengo el ultimo digito.
+    num_invertido = (
+        num_invertido * 10 + digito
+    )  # Voy acumulando los digitos de atras a delante.
+    numero = (
+        numero // 10
+    )  # Al numero ingresado por el usuario le voy quitando el ultimo digito.
+print(f"Numereo invertido: {num_invertido}")
